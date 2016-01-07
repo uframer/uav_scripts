0ea281ad5a4225d22fd4077179dc5c0509d566e8
Jiao Ye <uframer@gmail.com>
2016-01-07 17:57:23 +0800

    Switch to "build from source" method
    Build from source because ROS armhf prebuilts can only support 14.04.

--- sync_ros_bbb.sh
+++ sync_ros_bbb.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-
-sudo update-locale LANG=C LANGUAGE=C LC_ALL=C LC_MESSAGES=POSIX
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
-wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
+echo "You must enable universe, multiverse and restricted sources in /etc/apt/sources.list before running this script. Press ENTER to continue, or press CTRL-C to quit:"
+read
 sudo apt-get update
-sudo apt-get install ros-jade-ros-base
+sudo apt-get install python-pip
+sudo pip install --upgrade setuptools
 
-sudo apt-get install python-rosdep
+# Build from source because ROS armhf prebuilts can only support 14.04
+sudo pip install -U rosdep rosinstall_generator wstool rosinstall
+
 sudo rosdep init
 rosdep update
-
-echo "source /opt/ros/jade/setup.bash" >> ~/.bashrc
-source ~/.bashrc
-
-sudo apt-get install python-rosinstall
-
