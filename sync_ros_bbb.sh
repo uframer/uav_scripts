47c267f206f5b42f471f3ee830569f865c829a21
Jiao Ye <uframer@gmail.com>
2016-01-08 09:04:18 +0800

    Append more actions to download and install ROS

--- sync_ros_bbb.sh
+++ sync_ros_bbb.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-echo "You must enable universe, multiverse and restricted sources in /etc/apt/sources.list before running this script. Press ENTER to continue, or press CTRL-C to quit:"
+echo "You must enable universe, multiverse and restricted sources in /etc/apt/sources.list before running this script or you fill encounter install failure of some packages. Press ENTER to continue, or press CTRL-C to quit:"
 read
+
+if  [ $# != 1 ] ; then
+    printf "Usage:\n\t$0 <workspace dir>"
+    exit 1
+fi
+
+workspace_dir=$1
+
 sudo apt-get update
 sudo apt-get install python-pip
 sudo pip install --upgrade setuptools
@@ -10,3 +18,27 @@ sudo pip install -U rosdep rosinstall_generator wstool rosinstall
 
 sudo rosdep init
 rosdep update
+
+mkdir -p ${workspace_dir}
+cd ${workspace_dir}
+
+# Uncomment the following lines to install ROS-Comm: ROS package, build, and communication libraries. No GUI tools.
+rosinstall_generator ros_comm --rosdistro jade --deps --wet-only --tar > jade-ros_comm-wet.rosinstall
+wstool init -j8 src jade-ros_comm-wet.rosinstall
+
+# Uncomment the following lines to install Desktop: ROS, rqt, rviz, and robot-generic libraries
+#rosinstall_generator desktop --rosdistro jade --deps --wet-only --tar > jade-desktop-wet.rosinstall
+#wstool init -j8 src jade-desktop-wet.rosinstall
+
+# Uncomment the following lines to install Desktop-Full: ROS, rqt, rviz, robot-generic libraries, 2D/3D simulators, navigation and 2D/3D perception
+# rosinstall_generator desktop_full --rosdistro jade --deps --wet-only --tar > jade-desktop-full-wet.rosinstall
+# wstool init -j8 src jade-desktop-full-wet.rosinstall
+
+# Check build dependencies
+rosdep install --from-paths src --ignore-src --rosdistro jade -y
+
+# Build!
+./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release
+
+# Source environments, you may want to add this to your .bashrc or .bash_profile
+source ~/ros_catkin_ws/install_isolated/setup.bash
